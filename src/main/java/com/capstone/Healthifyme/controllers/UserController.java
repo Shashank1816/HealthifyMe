80883f72207658960104879a93cd2e344e48c51f
Aman Sahu <Aman.Sahu@BRILLIO.COM>
2022-08-16 11:49:00 +0530

    connection to angular

--- src/main/java/com/capstone/Healthifyme/controllers/UserController.java
+++ src/main/java/com/capstone/Healthifyme/controllers/UserController.java
@@ -3,13 +3,17 @@ package com.capstone.Healthifyme.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.capstone.Healthifyme.entities.User;
 import com.capstone.Healthifyme.repos.UserRepo;
 
 @RestController
+@CrossOrigin(origins="http://localhost:4200")
+//@RequestMapping("/brillio")
 public class UserController {
 	@Autowired
 	UserRepo userRepo;
@@ -17,6 +21,7 @@ public class UserController {
 	// just creating a sample program for getting all the users
 	@GetMapping(path = "/users")
 	public List<User> getUsers() {
+		System.out.println("getting users");
 		return userRepo.findAll();// we need to return user.findAll();
 	}
 
