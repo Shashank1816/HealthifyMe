ec94c9622567b7b5dd7d0492a180ef911a36da6f
Aman Sahu <Aman.Sahu@BRILLIO.COM>
2022-08-19 15:06:11 +0530

    Other controllers

--- src/main/java/com/capstone/Healthifyme/controllers/PlanController.java
+++ src/main/java/com/capstone/Healthifyme/controllers/PlanController.java
@@ -3,13 +3,17 @@ package com.capstone.Healthifyme.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.capstone.Healthifyme.entities.Plan;
 import com.capstone.Healthifyme.repos.PlanRepo;
 
 @RestController
+@CrossOrigin(origins="http://localhost:4200")
 public class PlanController {
 	
 	@Autowired
@@ -18,6 +22,13 @@ public class PlanController {
 	@GetMapping(path = "/plans")
 	public List<Plan> getPlans(){
 		return planRepo.findAll();
+	}
+	
+	@PostMapping(path="/plans")
+	public void addUser(@RequestBody Plan plan) {
+		System.out.println("Adding Plan : ");
+		System.out.println(plan);
+		planRepo.save(plan);
 	}
 
 }
