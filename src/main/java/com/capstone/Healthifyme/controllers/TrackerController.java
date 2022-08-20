4094823b10176b0908dfee3cd12483775b60a12e
Aman Sahu <Aman.Sahu@BRILLIO.COM>
2022-08-20 14:46:27 +0530

    minor changes

--- src/main/java/com/capstone/Healthifyme/controllers/TrackerController.java
+++ src/main/java/com/capstone/Healthifyme/controllers/TrackerController.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -30,8 +32,9 @@ public class TrackerController {
 	private WeightRepo weightRepo;
 	
 	@GetMapping(path = "")
-	public void getTrackers() {
+	public String getTrackers() {
 		System.out.println("Trackers endpoint");
+		return "Nothing to show here";
 	}
 	
 	@GetMapping(path = "/calorie")
@@ -40,15 +43,33 @@ public class TrackerController {
 		return calorieRepo.findAll();
 	}
 	
+//	@PostMapping(path = "/calorie")
+//	public Calorie addCalorie(@RequestBody Calorie calorie) {
+//		System.out.println("Adding Calorie");
+//		return calorieRepo.save(calorie);
+//	}
+	
 	@GetMapping(path = "/water")
 	public List<Water> getWater() {
 		System.out.println("Water endpoint");
 		return waterRepo.findAll();
 	}
 	
+//	@PostMapping(path = "/water")
+//	public Water addWater(@RequestBody Water water) {
+//		System.out.println("Adding Water");
+//		return waterRepo.save(water);
+//	}
+	
 	@GetMapping(path = "/weight")
 	public List<Weight> getWeight() {
 		System.out.println("Weight endpoint");
 		return weightRepo.findAll();
-	} 
+	}
+	
+//	@PostMapping(path = "/weight")
+//	public Weight addWeight(@RequestBody Weight weight) {
+//		System.out.println("Adding Weight");
+//		return weightRepo.save(weight);
+//	}
 }
